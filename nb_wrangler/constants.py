203ecf2a59821838a2cbb223f6ffeca4c0d436ec
Todd Miller <jaytmiller@gmail.com>
2025-09-03 17:20:12 -0400

    Added NBW_MAMBA_PATH env var

--- nb_wrangler/constants.py
+++ nb_wrangler/constants.py
@@ -9,10 +9,10 @@ __version__ = "0.2.0"
 # Path constants
 HOME = Path(os.environ.get("HOME", "."))
 NBW_ROOT = Path(os.environ.get("NBW_ROOT", HOME / ".nbw-live"))
-NBW_MM = NBW_ROOT / "mm"
+NBW_MM = Path(os.environ.get("NBW_MM", NBW_ROOT / "mm"))
 NBW_PANTRY = Path(os.environ.get("NBW_PANTRY", HOME / ".nbw-pantry"))
 REPOS_DIR = Path("./references")
-DEFAULT_MICROMAMBA_PATH = NBW_MM / "bin" / "micromamba"
+DEFAULT_MICROMAMBA_PATH = Path(os.environ.get("NBW_MAMBA_PATH", NBW_MM / "bin" / "micromamba"))
 
 BUILTIN_PACKAGES = ["__future__", "builtins", "sys", "os", "copy"]
 
