773549ea6bad326c2dbf906314a8a0f4e7e8754f
jaytmiller <jmiller@stsci.edu>
2026-01-29 08:26:18 -0500

    Added --env-user-registry and made default kernel install location the system version and not the user version under $HOME unless the switch is given.  This prevents post-start-hook kernel setup (which may create $HOME symlinks)

--- nb_wrangler/environment.py
+++ nb_wrangler/environment.py
@@ -302,8 +302,9 @@ class EnvironmentManager(WranglerConfigurable, WranglerLoggable):
         env_switches = ""
         for key, value in env_vars.items():
             env_switches += f"--env '{key}' '{value}' "
+        registry = "--user" if self.config.env_user_registry else ""
         cmd = self._condition_cmd(
-            f"python -m ipykernel install --user --name '{env_name}' --display-name '{display_name}' {env_switches}"
+            f"python -m ipykernel install {registry} --name '{env_name}' --display-name '{display_name}' {env_switches}"
         )
         result = self.env_run(env_name, cmd, check=False)
         return self.handle_result(
