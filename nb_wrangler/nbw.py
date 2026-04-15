1560c126e077299bc7b65e56c48aed3b7e8ff095
jaytmiller <jmiller@stsci.edu>
2026-04-15 16:44:43 -0400

    Blacked

--- nb_wrangler/nbw.py
+++ nb_wrangler/nbw.py
@@ -2,6 +2,7 @@ from pathlib import Path
 import subprocess
 import sys
 
+
 def main():
     script = Path(__file__).with_name("nb-wrangler")
     raise SystemExit(subprocess.call(["bash", str(script), *sys.argv[1:]]))
